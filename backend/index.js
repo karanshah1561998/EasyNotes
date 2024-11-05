d72b0f009db79d5178d02470a060161e79e63c0f
Karan Shah <karanshah1561998@gmail.com>
2024-11-05 16:28:09 -0500

    Backend - Get All Notes API

--- backend/index.js
+++ backend/index.js
@@ -206,6 +206,26 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
 
 });
 
+// Get All Notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+    const { user } = req.user.user;
+
+    try {
+        const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+
+        return res.json({
+            error: false,
+            notes,
+            message: "All notes retrieved successfully",
+        });
+    } catch (error) {
+        console.error("Error saving note:", error);
+        return res
+            .status(500)
+            .json({ error: true, message: "Internal Server Error" });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
