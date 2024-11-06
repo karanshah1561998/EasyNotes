05b30c974f0556aae8e35fd71347808bac223390
Karan Shah <karanshah1561998@gmail.com>
2024-11-05 19:02:52 -0500

    Frontend API Integration - Create account API integration && All Notes API integration && Add New Note API integration

--- frontend/easynotes/src/pages/Home/Home.jsx
+++ frontend/easynotes/src/pages/Home/Home.jsx
@@ -15,8 +15,17 @@ const Home = () => {
         data: null,
     });
 
+    const [allNotes, setAllNotes] = useState([]);
     const [userInfo, setUserInfo] = useState(null);
     const navigate = useNavigate();
+
+    const handleEdit = (noteDetails) => {
+        setOpenAddEditModal({
+            isShown: true, 
+            data: noteDetails,
+            type: 'edit',
+        });
+    };
 
     const getUserInfo = async () => {
         try {
@@ -32,7 +41,19 @@ const Home = () => {
         }
     };
 
+    const getAllNotes = async () => {
+        try {
+            const response = await axiosInstance.get("/get-all-notes");
+            if (response.data && response.data.notes) {
+                setAllNotes(response.data.notes);
+            }
+        } catch (error) {
+            console.log("An unexpected error occurred. Please try again.");
+        }
+    }
+
     useEffect(() => {
+        getAllNotes();
         getUserInfo();
         return () => {};
     }, []);
@@ -43,16 +64,19 @@ const Home = () => {
 
             <div className='container mx-auto'>
                 <div className='grid grid-cols-3 gap-4 mt-8'>
-                    <NoteCard
-                        title='Meeting on 7th April'
-                        date='3rd April 2024'
-                        content='Meeting on 7th April Meeting on 7th April'
-                        tags='#Meeting'
-                        isPinned={true}
-                        onEdit={()=>{}}
-                        onDelete={()=>{}}
-                        onPinNote={()=>{}}
+                    {allNotes.map((item, index) => (
+                        <NoteCard
+                            key={item._id}
+                            title={item.title}
+                            date={item.createdOn}
+                            content={item.content}
+                            tags={item.tags}
+                            isPinned={item.isPinned}
+                            onEdit={() => handleEdit(item)}
+                            onDelete={()=>{}}
+                            onPinNote={()=>{}}
                     />
+                    ))}
                 </div>
             </div>
 
@@ -84,6 +108,7 @@ const Home = () => {
                             data: null
                         });
                     }}
+                    getAllNotes={getAllNotes}
                 />
             </Modal>
 
